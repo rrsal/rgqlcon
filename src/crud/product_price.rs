ee4efc06d878c5c2f3ee53cdd3e87689895e80bd
raushanraja <vraushanraja@gmail.com>
2021-11-29 18:52:32 +0000

    Add product_price

---
+++ src/crud/product_price.rs
@@ -0,0 +1,84 @@
+use crate::crud::base::{getCurrentDate, CO};
+use crate::gql::root::Ctx;
+use crate::models::product_price::{NewProductPrice, ProductPrice, UpdateProductPrice};
+use chrono::*;
+use diesel::prelude::*;
+use juniper::FieldResult;
+
+impl CO for ProductPrice{
+    type All = Vec<ProductPrice>;
+    type Get = FieldResult<ProductPrice>;
+    type Update = UpdateProductPrice;
+    type New = NewProductPrice;
+
+    fn all(&self, ctx: &Ctx) -> Self::All {
+        use crate::schema::product_price::dsl:*;
+        let connection = ctx.db.get().unwrap();
+
+        product_price
+            .limit(100)
+            .load::<Self>(&connection)
+            .expect("Error laoding product price")
+    }
+
+    
+    fn by_id(&self, ctx: &Ctx, id: String) -> Self::Get {
+        use crate::schema::product_price::dsl::*;
+        let connection = ctx.db.get().unwrap();
+
+        let result = product_price
+            .filter(price_id.eq(id))
+            .first::<Self>(&connection)?;
+        Ok(result)
+    }
+
+    fn create(&self, ctx: &Ctx, new_price: Self::New) -> Self::Get {
+        use crate::schema::product_price::dsl::*;
+        let connection = ctx.db.get().unwrap();
+        let id = uuid::Uuid::new_v4().to_string();
+        let now = getCurrentDate();
+        let new = Self::new(id,now,new_price);
+
+        let result = diesel::insert_into(product_price)
+            .values(new)
+            .get_result::<Self>(&connection);
+        
+        match result {
+            Ok(t) => Ok(t),
+            Err(e) => FieldResult::Err(juniper::FieldError::from(e)),
+        }
+
+    }
+
+    fn update(&self, ctx: &Ctx, update_price: Self::Update) -> Self::Get {
+        use crate::schema::product_price::dsl::*;
+        let connection = ctx.db.get().unwrap();
+
+        let result = diesel::update(update_price)
+            .filter(price_id.eq(id))
+            .set(update_price)
+            .get_result::<Self>(&connection)?;
+        Ok(result)
+    }
+
+    fn delete(&self, ctx: &Ctx, id: String) -> Self::Get {
+        use crate::schema::product_price::dsl::*;
+        let connection = ctx.db.get().unwrap();
+
+        let result = diesel::delete(product_price)
+            .filter(price_id.eq(id))
+            .get_result::<Self>(&connection)?;
+        Ok(result)
+    }
+
+}
+
+impl Default for ProductPrice {
+    fn default() -> Self {
+        Self {
+            price_id: String::from(""),
+            date_from: NaiveDate::from_ymd(2015, 6, 3).and_hms(9, 10, 11),,
+            product_id: String::from(""),
+            price: 0.0
+        }
+    }
--- src/models/product_price.rs
+++ src/models/product_price.rs
@@ -1,34 +1,38 @@
 use crate::schema::*;
 use chrono::NaiveDateTime;
-use juniper::{GraphQLObject, GraphQLInputObject};
+use juniper::{GraphQLInputObject, GraphQLObject};
 
-#[derive(Queryable,Insertable,GraphQLObject)]
+#[derive(Queryable, Insertable, GraphQLObject)]
 #[table_name = "product_price"]
-pub struct ProductPrice{
+pub struct ProductPrice {
     pub date_from: NaiveDateTime,
     pub product_id: String,
     pub price: f64,
-    pub price_id: String
+    pub price_id: String,
 }
 
 #[derive(GraphQLInputObject)]
 #[graphql(description = "New Product Price")]
 pub struct NewProductPrice {
     pub product_id: String,
-    pub price: f64
+    pub price: f64,
 }
 
-#[derive(GraphQLInputObject)]
+#[derive(GraphQLInputObject, AsChangeset)]
 #[graphql(description = "Updated Product Price")]
-pub struct UpdateProductPrice{
-    pub product_id: String,
-    pub price: f64
-}
-
-#[derive(AsChangeset)]
-#[table_name = "product_price"]
-pub struct UpdateProductReviewDB{
+pub struct UpdateProductPrice {
     pub date_from: NaiveDateTime,
     pub product_id: String,
-    pub price: f64
+    pub price: f64,
+}
+
+impl ProductPrice {
+    pub fn new(id: String, date_from: NaiveDateTime, new_price: NewProductPrice) -> Self {
+        Self {
+            price_id: id,
+            date_from: date_from,
+            product_id: new_price.product_id,
+            price: new_price.price,
+        }
+    }
 }
